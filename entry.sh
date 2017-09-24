902aaa676ee63b143ae61d93e292a352c2457c7a
Eric Stoekl <ems5311@gmail.com>
2017-09-23 18:24:57 -0700

    mp4 output

--- entry.sh
+++ entry.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+nano=`date +%s%N`
 
 while read line
 do
   echo "$line"
 done < "${1:-/dev/stdin}"
 
-youtube-dl $line --no-warnings --quiet -o -
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' $line --no-warnings --quiet -o $nano.mp4
+cat $nano.mp4 > /dev/stdout
